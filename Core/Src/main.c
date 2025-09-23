0bd5dddcd0e21fab2e0f40855f1abb149f537ef8
Hoomas <mackarhoomas@gmail.com>
2025-09-23 10:53:39 +0700

    1 practise

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,4 +1,16 @@
-int main(void){
-    while(1){}
+#include <stdint.h>
 
+int main(void){
+
+    *(uint32_t*)(0x40023800 + 0x30) |= 0x02; //TACT B EN 
+    //Для включения регистров посылается 16-ти ричная форма двоичного числа нужных регстров(прим. третий и второй регистры, будет число 110 -> 6)
+    *(uint32_t*)(0x40020400 + 0x00) |= 0x4000; // GPIO B EN
+    *(uint32_t*)(0x40020400 + 0x04) = 0x00; //PURDR to zeroes
+    *(uint32_t*)(0x40020400 + 0x08) |= 0x4000; // Speed setup
+    *(uint32_t*)(0x40020400 + 0x18) = 0x80; //Light on 
+   
+ 
+    while(1){}
+  git config --global user.email "you@example.com"
+  
 }
