68a37f833398dd52df298ff3f95ec5c748629e36
Hoomas <mackarhoomas@gmail.com>
2025-11-10 18:39:59 +0700

    First lab done

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -4,9 +4,18 @@
 
 #include "../../CMSIS/Devices/STM32F4xx/Inc/stm32f4xx.h"
 
+//led pins PB10(69), PB11(70), PB12(73)
+//button pins PB10(69), PB6(136)
+
+
 void GPIO_init__Memory(void);
-void GPIO_init_With_Myself_Macros (void);
-void GPIO_init_With_Myself_Macros2 (void);
+
+
+void Led_init(void);
+void Buttons_Init(void);
+void PB10_out(void);
+void PB10_inp(void);
+
 
 #define RCC_AHB1ENR         *(uint32_t *)(0x40023800UL + 0x30UL)
 #define RCC_GPIOB_EN        0x02UL
@@ -15,21 +24,49 @@ void GPIO_init_With_Myself_Macros2 (void);
 #define GPIOB_MODER         *(uint32_t *)(0x40020400UL + 0x00UL)
 #define GPIOB_OTYPER        *(uint32_t *)(0x40020400UL + 0x04UL)
 #define GPIOB_OSPEEDR       *(uint32_t *)(0x40020400UL + 0x08UL)
+#define GPIOB_PUPDR         *(uint32_t *)(0x40020400UL + 0x0CUL)
 #define GPIOB_BSRR          *(uint32_t *)(0x40020400UL + 0x18UL)
 
 #define GPIOC_IDR           *(uint32_t *)(0x40020800UL + 0x10UL)
 
-#define GPIO_PIN_MED_14     0x10000000UL
-#define GPIO_PIN_OUT_14     0x10000000UL
-#define GPIO_PIN_RESET_14   0x40000000UL
-#define GPIO_PIN_SET_14     0x4000UL
 
-#define GPIO_PIN_MED_7      0x4000UL
-#define GPIO_PIN_OUT_7      0x4000UL
+#define GPIO_PIN_OUT_6      0x00001000UL  //13:12
+#define GPIO_PIN_MED_6      0x00001000UL  // medium speed
+#define GPIO_PIN_SET_6      0x00000040UL  
+#define GPIO_PIN_RESET_6    0x00400000UL 
+
+
+#define GPIO_PIN_OUT_10     0x00100000UL  //21:20
+#define GPIO_PIN_MED_10     0x00100000UL  // medium speed
+#define GPIO_PIN_SET_10     0x00000400UL 
+#define GPIO_PIN_RESET_10   0x04000000UL 
+#define GPIO_PIN_READ_10    0x00000400UL
+#define GPIO_PIN_PULLDN_10  0x00200000UL 
+#define GPIO_PIN_INP_NI_10  0x00300000UL // not inversed
+
+
+
+#define GPIO_PIN_OUT_11     0x400000UL // 22:23
+#define GPIO_PIN_MED_11     0x400000UL // medium speed
+#define GPIO_PIN_SET_11     0x800UL 
+#define GPIO_PIN_RESET_11   0x8000000UL
+
+#define GPIO_PIN_OUT_12     0x01000000UL  //25:24
+#define GPIO_PIN_MED_12     0x01000000UL  // medium speed
+#define GPIO_PIN_SET_12     0x00001000UL  
+#define GPIO_PIN_RESET_12   0x10000000UL  
+
+#define GPIO_PIN_OUT_15     0x40000000UL  // 31:30
+#define GPIO_PIN_MED_15     0x40000000UL  // medium speed
+#define GPIO_PIN_SET_15     0x00008000UL  
+#define GPIO_PIN_RESET_15   0x80000000UL  
+
+
+
+
+
 #define GPIO_OFF            0x00UL
-#define GPIO_PIN_RESET_7    0x800000UL
-#define GPIO_PIN_SET_7      0x80UL
-#define GPIO_PIN_13         0x2000UL
 
 #define BIT_SET(REG, BIT)   ((REG) |= (BIT))
 #define BIT_READ(REG, BIT)  ((REG) & (BIT))
+
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,21 +1,124 @@
 #include "main.h"
 
+
+static void delay_cycles(volatile uint32_t t) //time in ms
+{
+    while (t--) __NOP();
+}
+
+static uint8_t debounce(uint32_t bitmask)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        if (BIT_READ(GPIOB->IDR, bitmask))
+            return 0;
+        delay_cycles(5000);
+    }
+    return 1;
+}
+
+
+uint8_t btn1 = 0;
+uint8_t btn2 = 0;
+uint8_t btn2_state = 0;   // 0 = PB10 input, 1 = PB10 output
+uint8_t led_flag = 0;
 int main(void)
 {
-    GPIO_init_With_Myself_Macros ();
-    GPIO_init_With_Myself_Macros2 ();
+    Led_init();
+    Buttons_Init();
+    SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR10);    
+
     while (1)
     {
-        BIT_SET (GPIOB_BSRR, GPIO_PIN_SET_14);
-        if (BIT_READ(GPIOC_IDR, GPIO_PIN_13)) // опереатор if, в котором логически умножается IDR на значение
+        
+        if (debounce(GPIO_IDR_ID6))
         {
-            BIT_SET(GPIOB_BSRR, GPIO_PIN_SET_7);
-            BIT_SET(GPIOB_BSRR, GPIO_PIN_RESET_14);   // Включает светодиод BSSR, если нажата кнопка
+            btn2 = 1;
+                
+                btn2_state = !btn2_state;
+                delay_cycles(500);
+        }
+        btn2 = 0;
+
+        if (btn2_state == 0)
+        {
+ 
+            PB10_inp();
+
+            if (debounce(GPIO_IDR_ID10))
+            {
+                btn1++;
+                
+                    switch(btn1)
+                    {
+                    case 1:
+                        SET_BIT(GPIOB->BSRR, GPIO_PIN_SET_11);
+                        SET_BIT(GPIOB->BSRR,  GPIO_PIN_RESET_12);
+                        break;
+                    case 2:
+                        SET_BIT(GPIOB->BSRR, GPIO_PIN_SET_11);
+                        SET_BIT(GPIOB->BSRR,  GPIO_PIN_SET_12);
+                        break;
+                    case 3:
+                        btn1 = 0;
+                        SET_BIT(GPIOB->BSRR,  GPIO_PIN_RESET_11);
+                        SET_BIT(GPIOB->BSRR,  GPIO_PIN_RESET_12);
+                        break;
+                    }
+                    delay_cycles(5000);
+                
+            }
+            
         }
         else
         {
-            BIT_SET(GPIOB_BSRR, GPIO_PIN_SET_14);
-            BIT_SET(GPIOB_BSRR, GPIO_PIN_RESET_7);
+            PB10_out();
+
+            BIT_SET(GPIOB_BSRR, GPIO_BSRR_BS10);
+            BIT_SET(GPIOB_BSRR, GPIO_BSRR_BR11);
+            BIT_SET(GPIOB_BSRR, GPIO_BSRR_BR12);
+
+    
+            while (1)
+            {
+                
+                if (debounce(GPIO_IDR_ID6))
+                {
+                        btn2 = 1;
+                        BIT_SET(GPIOB_BSRR, GPIO_BSRR_BR10);
+                        PB10_inp();
+                        btn2_state = 0;
+                        delay_cycles(500000);
+                        break;
+                }
+                
+            }
         }
     }
 }
+
+/*
+uint8_t flag = 0 ;
+int main()
+{
+    Led_init();
+    Buttons_Init();
+    PB10_out();
+   
+    while(1)
+    {
+        
+           
+             SET_BIT(GPIOB->BSRR, GPIO_PIN_SET_12);
+             SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS11);
+             SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS10);
+        
+
+        SET_BIT(GPIOB->BSRR, GPIO_PIN_RESET_12);
+        if(!READ_BIT(GPIOB->IDR, GPIO_IDR_ID6))
+        {flag++;}
+
+    }
+    
+} 
+    */
